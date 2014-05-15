796b3c41aa5805a5d0c39b5727ad79d88ec41231
Антон Голомазов <cesear@bk.ru>
2014-05-15 17:53:01 +0400

    Add about page

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -2,8 +2,17 @@
 
 class Main extends CI_Controller{
 
+
     public function index(){
         $this->load->view('home_view');
+    }
+
+    public function about(){
+        $data = array();
+        $data['name'] = "Anton";
+        $data['surname'] = "Golomazov";
+        $data['age'] = 26;
+        $this->load->view("about_view", $data);
     }
 
 }
---
+++ application/views/about_view.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Страница О нас</title>
+</head>
+<body>
+    <h1>Страница О Нас</h1>
+
+    <p>
+        Меня зовут <?=$name.' '.$surname;?><br />
+        Мне <?=$age;?> лет
+    </p>
+</body>
+</html>
