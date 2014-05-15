7f5f964c5c7767b88cfe66c6c51a026daf86a110
Антон Голомазов <cesear@bk.ru>
2014-05-15 17:23:40 +0400

    Change config files

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'cesar';
+$db['default']['password'] = '997583';
+$db['default']['database'] = 'cilessons';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
